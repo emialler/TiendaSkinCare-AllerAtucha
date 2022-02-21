1f5787fcc943e8214d56d12b57f54f3580f8c91b
Emilia Aller Atucha <emi_aller@hotmail.com>
2022-02-21 10:53:39 -0300

    prueba de rama

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+const NavBar = () => {
+    return (
+        <>
+        <header>
+            <nav>
+                <h1>PURE - SkinCare</h1>
+                <ul>
+                    <li><a href="">Productos</a></li>
+                    <li><a href="">Tips de SkinCare</a></li>
+                    <li><a href="">Carrito</a></li>
+                </ul>
+            </nav>
+        </header>
+        </>
+    )
+}
+
+export default NavBar
