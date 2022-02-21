cc876106e959288a6c6d0297dad553aa3d76dfa6
Emilia Aller Atucha <emi_aller@hotmail.com>
2022-02-21 11:31:56 -0300

    Creo Componente NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,12 +3,17 @@ const NavBar = () => {
         <>
         <header>
             <nav>
-                <h1>PURE - SkinCare</h1>
-                <ul>
-                    <li><a href="">Productos</a></li>
-                    <li><a href="">Tips de SkinCare</a></li>
-                    <li><a href="">Carrito</a></li>
-                </ul>
+                <div>
+                    <img src="pure.jpg"></img>
+                    <h1>PURE - SkinCare</h1>
+                </div>
+                <div>
+                    <ul>
+                        <li><a href="">Productos</a></li>
+                        <li><a href="">Tips de SkinCare</a></li>
+                        <li><a href="">Carrito</a></li>
+                    </ul>
+                </div>
             </nav>
         </header>
         </>
